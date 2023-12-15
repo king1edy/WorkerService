85fcac773941d6f276912529b11b0d717df84b4d
Godswill David <davidgodswill@gmail.com>
2023-12-15 15:56:28 +0100

    updated codebase code clean-up Added Entity class

---
+++ Src/WorkerService/Models/Shift.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkerService.API.Models
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int WorkerId { get; set; }
+        public Worker Worker { get; set; }
+    }
+}
---
+++ Src/WorkerService/Models/Worker.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkerService.API.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Shift> Shifts { get; set; }
+    }
+}
